7fbd1d12cf8e14ec34b0158d7ac0c80dd36669ac
Fallapi <f.ilic@st.hanze.nl>
2025-07-10 17:00:32 +0200

    added menubar

---
+++ src/app/components/menu-bar/menu-bar.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { MenubarModule } from 'primeng/menubar';
+
+
+@Component({
+  selector: 'app-menu-bar',
+  imports: [MenubarModule],
+  templateUrl: './menu-bar.html',
+  styleUrl: './menu-bar.css'
+})
+export class MenuBar {
+  items = [
+    {
+      label: 'Home',
+      routerLink: [''],
+      icon: 'pi pi-home',
+    },
+    {
+      label: 'Players',
+      routerLink: ['/admin/players'],
+      icon: 'pi pi-address-book'
+    },
+    {
+      label:'Games',
+      routerLink: ['/admin/games'],
+      icon: 'pi pi-gauge'
+    },
+    {
+      label: 'Dates',
+      routerLink: ['/admin/dates'],
+      icon: 'pi pi-calendar-clock'
+    }
+  ]
+}
