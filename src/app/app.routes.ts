31ddc2e6d9c818b4951ab41d6c7a051289c4aef6
Fallapi <f.ilic@st.hanze.nl>
2025-07-10 15:34:21 +0200

    basic pages + routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Players } from './admin/players/players'
+import { Games } from './admin/games/games'
+import { Dates } from './admin/dates/dates'
+import { Home } from './admin/home/home'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'admin/players', component: Players },
+  { path: 'admin/games', component: Games },
+  { path: 'admin/dates', component: Dates },
+  { path: 'admin', component: Home },
+  { path: '', redirectTo: 'admin', pathMatch: 'full' }
+
+];
