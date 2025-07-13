409797c8ecd6f9c9b0e7af4ccff5eeb8b471a94b
Fallapi <f.ilic@st.hanze.nl>
2025-07-13 23:29:37 +0200

    Login mooi gemaakt maar de css lukte me niet en had geen zin meer

--- src/app/admin/login/login.ts
+++ src/app/admin/login/login.ts
@@ -7,12 +7,28 @@ import {InputText} from 'primeng/inputtext';
 import {HttpClient, HttpClientModule } from '@angular/common/http';
 import {AggregateService} from '../../services/aggregate-service';
 import {LoginService} from '../../services/login.service';
+import { ToastModule } from 'primeng/toast';
+import { MessageModule } from 'primeng/message';
+import { MessageService } from 'primeng/api';
+
+
+
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [InputGroupAddonModule, InputGroupModule, PasswordModule, FormsModule, InputText, HttpClientModule],
+  imports: [
+    InputGroupAddonModule,
+    InputGroupModule,
+    PasswordModule,
+    FormsModule,
+    InputText,
+    HttpClientModule,
+    ToastModule,
+    MessageModule
+  ],
+  providers: [MessageService],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
@@ -21,7 +37,9 @@ export class Login {
   password: string | undefined;
 
 
-  constructor(private LoginService: LoginService, private AggregateService: AggregateService) {}
+  constructor(private LoginService: LoginService,
+              private AggregateService: AggregateService,
+              private messageService: MessageService) {}
 
   logInAsAdmin():void {
     if (!this.username || !this.password) return;
@@ -33,18 +51,35 @@ export class Login {
         localStorage.setItem('jwt', res.token);
         this.checkAdmin();
       },
-      error: (err) => console.error('Login failed:', err)
-    });
+      error: (err) => {
+        console.error('Login failed:', err)
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Cannot login',
+          detail: 'Try again'
+        });
+      }
+    })
   }
 
   checkAdmin():void{
       this.AggregateService.getAggregateData().subscribe({
         next: (res) => {
           console.log('Login success:', res);
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Login success',
+            detail: 'Welcome admin '
+          });
         },
         error: (err) => {
           console.error('Login failed:', err);
           localStorage.removeItem('jwt');
+          this.messageService.add({
+            severity: 'error',
+            summary: 'LOL NO ADMIN',
+            detail: 'Take a walk buddy. you dont belong here'
+          });
         }
       })
   }
